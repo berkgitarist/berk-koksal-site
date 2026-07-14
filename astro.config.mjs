b32048859862e96a7f07e4d5f8278d3bfdafa250
Berk KÖKSAL <berkgitarist@gmail.com>
2026-07-14 15:01:57 +0300

    Add sitemap and robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://www.berkkoksal.com",
@@ -11,5 +12,8 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
 
-  integrations: [icon()],
+  integrations: [
+    icon(),
+    sitemap(),
+  ],
 });
